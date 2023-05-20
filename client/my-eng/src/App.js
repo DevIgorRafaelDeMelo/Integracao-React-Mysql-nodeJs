097f7fa5ac73dc2bebedc2994bf3abccf3082e4f
DevRafaelDeMelo <rafaelmelo765@gmail.com>
2023-05-19 21:28:27 -0300

    V1.1 19.05.23

--- client/my-eng/src/App.js
+++ client/my-eng/src/App.js
@@ -1,38 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
-  
-  const [values , setValues] = useState();
-  const handleChangeValues = (value) =>{
-    setValues((prevValue) =>({
+
+  const [values, setValues] = useState();
+  const handleChangeValues = (value) => {
+    setValues((prevValue) => ({
       ...prevValue,
       [value.target.name]: value.target.value,
     }));
   };
 
-  const handleClickButton = () =>{
-    console.log(values)
-  }
+  const handleClickButton = () => {
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      cost: values.cost,
+    }).then((resposta) => {
+      console.log(resposta);
+    });
+  };
 
   return (
     <div className="App">
       <div className='card-input'>
         <div className='text-1'>Ingles</div>
         <input type='text'
-          name="name-eng"
+          name="name"
           className='name-eng'
           onChange={handleChangeValues}
         />
-        <br/>
+        <br />
         <div className='text-1'>tradução</div>
         <input type='text'
-          name="name-pt"
+          name="cost"
           className='name-pt'
           onChange={handleChangeValues}
         />
-        <br/>
-      <button className='btn-send' onClick={handleClickButton}>Cadastrar</button>
+        <br />
+        <button className='btn-send' onClick={handleClickButton}>Cadastrar</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
 
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "admin",
     database: "ingles",
-
 });
 
+app.use(express.json());
+app.use(cors());
 
-
-app.get('/', (req , res) =>{
-    db.query("INSERT INTO palavras ( nameeng , namept ) VALUES ( 'home' , 'casa');");
-    
+app.post("/register",(req ,res) => {
+    const nome = req.body.name;
+    const traduz = req.body.cost; 
+    db.query("SELECT * FROM palavras WHERE nameeng = ? ",[nome],
+    (err,result )=>{
+        if(err){
+            res.send(err);
+        }
+        if(result.length == 0){
+            db.query("INSERT INTO palavras (nameeng,namept) VALUES ( ?, ?)",[nome , traduz], (err ,result)=>{
+                if(err){
+                    res.send(err);
+                }
+                res.send({msg: "Cadastrado com sucesso!!!"});
+            })
+        }else(
+            res.send({msg: "Usuario já cadastrado!!!"})
+        )
+    });
 });
-
 
 app.listen(3001, () => {
     console.log("rodando servidor");
 });
+
